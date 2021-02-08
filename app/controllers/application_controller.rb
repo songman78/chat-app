14185ae41f3a2cf19baa2f46d5519875e41a6a09
songman78 <song.matsui@gmail.com>
2021-02-08 12:24:08 +0900

    deviseに関するビューファイルの配置

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  befor_action :authenticate_user!
+  befor_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:neme])
+  end
 end
