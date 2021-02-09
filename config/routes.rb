46c26b8a804b57e2caf1f5417006de53dd2cdf1d
songman78 <song.matsui@gmail.com>
2021-02-09 17:13:41 +0900

    重複していたファイルの編集

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,12 @@ Rails.application.routes.draw do
 
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
+
   resources :rooms, only: [:new, :create, :destroy] do
     resources :messages, only: [:index, :create ]
   end
+
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+
 end
